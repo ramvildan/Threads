6bb166c8f03bfcb6d33aad674fbaf8ddde877098
Ramil Vildanov <ramvildan.work@gmail.com>
2022-08-29 17:42:22 +0300

    Another try

--- src/main/java/threadsHW/Main.java
+++ src/main/java/threadsHW/Main.java
@@ -8,46 +8,58 @@ import java.util.ArrayList;
 
 public class Main {
 
+    private static int amountOfNumbers = 1_000_000;
+    private static int numberOfThreads = 4;
+    private static int workOfThreads = amountOfNumbers / numberOfThreads;
+    private static ArrayList<Thread> threads = new ArrayList<>();
+    private static double[] arrayResults = new double[amountOfNumbers];
+    private static ArrayList<Double> results = new ArrayList<>();
     public static void main(String[] args) throws InterruptedException {
 
         long startTime = System.currentTimeMillis();
 
-        int amountOfNumbers = 1_000_000;
-        int numberOfThreads = 4;
-        int workOfThreads = amountOfNumbers / numberOfThreads;
-
-        ArrayList<Thread> threads = new ArrayList<>();
-        ArrayList<ArrayList<Double>> arrays = new ArrayList<>();
-
-        for (int threadNumber = 0; threadNumber < numberOfThreads; threadNumber++) {
-            int start = workOfThreads * threadNumber;
-            int end = start + workOfThreads;
-            ArrayList<Double> numbers = new ArrayList<>(workOfThreads);
-
-            Thread thread = new Thread(() -> {
-
-                for (int i = start; i <= end; i++) {
-                    double result = Math.sqrt(i);
-                    numbers.add(result);
-                }
-            });
-            threads.add(thread);
-            thread.start();
-            arrays.add(numbers);
+        for (int i = 0; i < numberOfThreads; i++) {
+            startThread(i);
         }
-        for (Thread thread : threads) {
-            thread.join();
+        joinAll();
+
+        for (int i = 0; i < amountOfNumbers; i++) {
+            double result = arrayResults[i];
+            results.add(result);
         }
 
-        File file = new File("SQRT.txt");
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-                        for (ArrayList<Double> line : arrays) {
-                            writer.write(String.valueOf(line));
-                            writer.newLine();
-                        }
-                    } catch (IOException exc) {}
+        fileWriter("SQRT.txt");
 
         long end = System.currentTimeMillis();
         System.out.println("Took: " + (end - startTime) + " ms");
+    }
+
+    public static void startThread(int threadNumber) {
+        Thread thread = new Thread(() -> {
+                for (int i = 1; i < workOfThreads; i++) {
+                    double SQRTResult = Math.sqrt(i);
+                    arrayResults[threadNumber * workOfThreads + i] = SQRTResult;
+                }
+        });
+        thread.start();
+        threads.add(thread);
+    }
+
+    private static void joinAll () throws InterruptedException {
+        for (Thread thread : threads) {
+            thread.join();
+        }
+    }
+
+    public static void fileWriter(String filepath) {
+        File file = new File(filepath);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (Double line : results) {
+                writer.write(String.valueOf(line));
+                writer.newLine();
+            }
+        } catch (IOException exc) {
+            throw new RuntimeException(exc);
+        }
     }
 }
